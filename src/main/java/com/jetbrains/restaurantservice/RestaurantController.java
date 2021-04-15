eada8a6b569f6c4fd90db0804e0aec2ce8b09536
Trisha Gee <trisha.gee@gmail.com>
2021-04-15 15:42:48 +0200

    Integration test for listing all restaurants. Currently checks for an empty list

--- src/main/java/com/jetbrains/restaurantservice/RestaurantController.java
+++ src/main/java/com/jetbrains/restaurantservice/RestaurantController.java
@@ -3,6 +3,7 @@ package com.jetbrains.restaurantservice;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -11,5 +12,10 @@ public class RestaurantController {
 	@GetMapping("/restaurant")
 	public Restaurant restaurant() {
 		return new Restaurant("Dalia's", "", 0, 0, List.of());
+	}
+
+	@GetMapping("/restaurants")
+	public List<Restaurant> getRestaurants() {
+		return new ArrayList<>();
 	}
 }
--- src/test/java/com/jetbrains/restaurantservice/RestaurantControllerTest.java
+++ src/test/java/com/jetbrains/restaurantservice/RestaurantControllerTest.java
@@ -1,11 +1,13 @@
 package com.jetbrains.restaurantservice;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.junit.jupiter.api.Assertions.fail;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -29,4 +31,27 @@ class RestaurantControllerTest {
                                                       "openingHours":[]}
                                                       """));
     }
+
+    @Test
+    @DisplayName("Should return a list of all restaurants in the database")
+    void shouldReturnAListOfAllRestaurantsInTheDatabase() throws Exception {
+        this.mockMvc.perform(get("/restaurants"))
+                    .andExpect(status().isOk())
+                    .andExpect(content().json("""
+                                                      []
+                                                      """));
+    }
+
+    // TODO: get all restaurants
+    // TODO: get a restaurant by ID
+    // TODO: update a restaurant's details
+    // TODO: delete a restaurant
+    // TODO: create a restaurant
+
+    // Restaurant Admins:
+    // {id: admin_id,
+    //  name: bob
+    //  password:
+    //  email: ,
+    //  restaurant_id: int}
 }
