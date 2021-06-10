87ab52b9620a6befba54e0c6922e6f113211c47a
Trisha Gee <trisha.gee@gmail.com>
2021-06-10 17:15:31 +0200

    Testcontainers works to run Integration test that requires MongoDB. Not sure this is idiomatic/best practice, doesn't feel quite right to have to manually call `start`

--- src/test/java/com/jetbrains/restaurantservice/RestaurantServiceIntegrationTest.java
+++ src/test/java/com/jetbrains/restaurantservice/RestaurantServiceIntegrationTest.java
@@ -1,12 +1,17 @@
 package com.jetbrains.restaurantservice;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
 
 import java.util.List;
 import java.util.Optional;
@@ -22,6 +27,19 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 class RestaurantServiceIntegrationTest {
+    @Container
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.4.5");
+
+    @BeforeAll
+    public static void startContainerAndPublicPortIsAvailable() {
+        mongoDBContainer.start();
+    }
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+    }
+
     @Autowired
     private MockMvc mockMvc;
 
