e656ca94b58fbfb334da2fef0553130438c77b29
Trisha Gee <trisha.gee@gmail.com>
2021-06-10 18:11:22 +0200

    Moved the configuration of a MongoDB docker container (via testcontainers) into an extension so it can be used by any test. Without having to create an abstract MongoDBIntegrationTest!

---
+++ src/test/java/com/jetbrains/restaurantservice/MongoDBContainerExtension.java
@@ -0,0 +1,22 @@
+package com.jetbrains.restaurantservice;
+
+import org.junit.jupiter.api.extension.AfterAllCallback;
+import org.junit.jupiter.api.extension.BeforeAllCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.testcontainers.containers.MongoDBContainer;
+
+public class MongoDBContainerExtension implements BeforeAllCallback, AfterAllCallback {
+    private MongoDBContainer mongoDBContainer;
+
+    @Override
+    public void beforeAll(ExtensionContext context) {
+        mongoDBContainer = new MongoDBContainer("mongo:4.4.5");
+        mongoDBContainer.start();
+        System.setProperty("spring.data.mongodb.uri", mongoDBContainer.getReplicaSetUrl());
+    }
+
+    @Override
+    public void afterAll(ExtensionContext context) {
+        mongoDBContainer.stop();
+    }
+}
--- src/test/java/com/jetbrains/restaurantservice/RestaurantServiceIntegrationTest.java
+++ src/test/java/com/jetbrains/restaurantservice/RestaurantServiceIntegrationTest.java
@@ -1,18 +1,13 @@
 package com.jetbrains.restaurantservice;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
-import org.testcontainers.containers.MongoDBContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.List;
 import java.util.Optional;
@@ -27,16 +22,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@Testcontainers
+@ExtendWith(MongoDBContainerExtension.class)
 class RestaurantServiceIntegrationTest {
-    @Container
-    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.4.5");
-
-    @DynamicPropertySource
-    static void setProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
-    }
-
     @Autowired
     private MockMvc mockMvc;
 
