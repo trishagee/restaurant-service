60b5916537b41c5261e5c96e989efc81bc067234
Trisha Gee <trisha.gee@gmail.com>
2021-08-13 15:19:49 +0100

    Updating MongoDB version

--- src/test/java/com/jetbrains/test/MongoDBContainerExtension.java
+++ src/test/java/com/jetbrains/test/MongoDBContainerExtension.java
@@ -10,7 +10,7 @@ public class MongoDBContainerExtension implements BeforeAllCallback, AfterAllCal
 
     @Override
     public void beforeAll(ExtensionContext context) {
-        mongoDBContainer = new MongoDBContainer("mongo:4.4.6");
+        mongoDBContainer = new MongoDBContainer("mongo:5.0.2");
         mongoDBContainer.start();
         System.setProperty("spring.data.mongodb.uri", mongoDBContainer.getReplicaSetUrl());
     }
