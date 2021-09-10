98072c64af5af6a3dee8f0b912031adfdfe0725c
Maarten Balliauw <maarten@maartenballiauw.be>
2021-09-10 13:31:41 +0200

    Build and publish container from package in automation job storage BLOSSOM-T-4

--- .space.kts
+++ .space.kts
@@ -1,4 +1,4 @@
-job("Build restaurant-service") {
+job("Build and publish restaurant-service container") {
     container(displayName = "Run mvn package", image = "maven:3.8.2-openjdk-16") {
         shellScript {
             content = """
@@ -10,6 +10,24 @@ job("Build restaurant-service") {
 
         service("mongo:5.0.2") {
             alias("mongodb")
+        }
+    }
+
+    docker(displayName = "Build and push container") {
+        beforeBuildScript {
+            content = """
+                mkdir target/
+                cp -r $mountDir/share/target/*.jar target
+            """.trimIndent()
+        }
+        build {
+            context = "."
+            file = "./Dockerfile"
+            labels["vendor"] = "GalacticTravels"
+        }
+
+        push("registry.jetbrains.team/p/blossom/containers/restaurant-service") {
+            tags("0.\$JB_SPACE_EXECUTION_NUMBER-\$JB_SPACE_GIT_BRANCH")
         }
     }
 }
