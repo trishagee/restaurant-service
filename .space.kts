5ffca78ac72c0d2fbb7f9809a6ac4c614576b5d9
Maarten Balliauw <maarten@maartenballiauw.be>
2021-09-10 13:25:44 +0200

    Package restaurant service BLOSSOM-T-4

--- .space.kts
+++ .space.kts
@@ -1,8 +1,8 @@
 job("Build restaurant-service") {
-    container(displayName = "Run mvn", image = "maven:3.8.2-openjdk-16") {
+    container(displayName = "Run mvn package", image = "maven:3.8.2-openjdk-16") {
         shellScript {
             content = """
-	            mvn clean install
+	            mvn clean package
             """.trimIndent()
         }
 
